8c0d0018852e72433128f2395bbc4b38ffc37aed
0xBit <73406588+0xBit-MC@users.noreply.github.com>
2021-03-19 17:20:42 -0700

    Removing Query from plugin and establishing JSON Parser from URL

--- src/dev/_2lstudios/cleanmotd/bungee/Main.java
+++ src/dev/_2lstudios/cleanmotd/bungee/Main.java
@@ -4,12 +4,28 @@ import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.plugin.Plugin;
 import net.md_5.bungee.api.plugin.PluginManager;
 
+import java.io.IOException;
+
 import java.util.concurrent.TimeUnit;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
 import dev._2lstudios.cleanmotd.bungee.commands.CleanMotDCommand;
-import dev._2lstudios.cleanmotd.bungee.listeners.MCQuery;
 import dev._2lstudios.cleanmotd.bungee.listeners.ProxyPingListener;
-import dev._2lstudios.cleanmotd.bungee.listeners.QueryResponse;
 import dev._2lstudios.cleanmotd.bungee.utils.ConfigurationUtil;
 import dev._2lstudios.cleanmotd.bungee.variables.Messages;
 import dev._2lstudios.cleanmotd.bungee.variables.Variables;
@@ -39,23 +55,33 @@ public class Main extends Plugin {
 	
 	 public void countPlayers() {
 	        getProxy().getScheduler().schedule(this, new Runnable() {
-	            @Override
+
+				@Override
 	            public void run() {
-	            	MCQuery mcQuery = new MCQuery("eu.earthpol.com", 25575);
-		        	QueryResponse response = mcQuery.basicStat();
-		        	playerCount = response.getOnlinePlayers();
-		        	playerCounter.setCount(playerCount);
+					JsonParser parser = new JsonParser();
+					
+					try {        
+			            URL oracle = new URL("https://earthpol.com/count/count.php"); // URL to Parse
+			            URLConnection yc = oracle.openConnection();
+			            BufferedReader in = new BufferedReader(new InputStreamReader(yc.getInputStream()));
+			           
+			            String inputLine;
+			            while ((inputLine = in.readLine()) != null) {              
+			                JsonObject a = (JsonObject) parser.parse(inputLine);
+			                String count = a.get("playerCount").toString();
+			                count = count.replace("\"","");
+			                playerCount = Integer.parseInt(count);
+			                playerCounter.setCount(playerCount);
+			            }
+			            in.close();
+			        } catch (FileNotFoundException e) {
+			            e.printStackTrace();
+			        } catch (IOException e) {
+			            e.printStackTrace();
+			        } catch (Exception e) {
+						e.printStackTrace();
+					}
 	            }
 	        }, 1, 5, TimeUnit.SECONDS);
 	    }
-	
-	
-	/*public static Runnable getPlayerCount =
-		    new Runnable(){
-		        public void run(){
-		        	MCQuery mcQuery = new MCQuery("us.earthpol.com", 25575);
-		        	QueryResponse response = mcQuery.basicStat();
-		        	playerCounter.setCount(response.getOnlinePlayers());
-		        }
-		    };*/
 }
